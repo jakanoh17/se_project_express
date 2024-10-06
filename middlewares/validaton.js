9032d49456caf7b3c7dee29e2da3c68a797529a6
jakanoh <jakanoh17@gmail.com>
2024-10-06 19:36:11 -0400

    fix typo in validation.js

--- middlewares/validaton.js
+++ middlewares/validaton.js
@@ -47,7 +47,7 @@ const validateNewUserData = celebrate({
   }),
 });
 
-const validateUpdatedUserData = celebrte({
+const validateUpdatedUserData = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     avatar: Joi.string().required().custom(validateURL).messages({
