9a0e434f9646055baddfa48fd0a117e02693d3fe
jakanoh <jakanoh17@gmail.com>
2024-10-12 15:41:00 -0400

    apply round 1 of edits

--- app.js
+++ app.js
@@ -3,24 +3,28 @@ const express = require("express");
 const cors = require("cors");
 const { errors } = require("celebrate");
 const mongoose = require("mongoose");
-
-const { mapAndSendErrors } = require("./utils/errors");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
+const helmet = require("helmet");
+const limiter = require("./utils/rateLimiter");
 
 mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 const app = express();
+const { PORT = 49152 } = process.env;
 
 const users = require("./routes/users");
 const clothingItems = require("./routes/clothingItems");
 const index = require("./routes/index");
-
-const { PORT = 49152 } = process.env;
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+const { mapAndSendErrors, notFound } = require("./utils/errors");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 app.use(cors());
 app.use(express.json());
 
-// REQUESTS
+// SECURITY
+app.use(limiter);
+app.use(helmet());
+
+// REQUESTS LOGS
 app.use(requestLogger);
 
 // CODE REVIEW CRASH TEST
@@ -34,6 +38,10 @@ app.use("/", index);
 app.use("/users", users);
 app.use("/items", clothingItems);
 
+app.use((req, res) => {
+  res.status(notFound.status).send({ message: notFound.message });
+});
+
 // ERRORS
 app.use(errorLogger);
 
@@ -41,9 +49,6 @@ app.use(errors());
 app.use((err, req, res, next) => {
   mapAndSendErrors(err, res);
 });
-// app.use((req, res) => {
-//   res.status(notFound.status).send({ message: notFound.message });
-// });
 
 app.listen(PORT, () => {
   console.log(`App listing on port: ${PORT}`);
